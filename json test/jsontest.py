8ba335ba751ffa43686a2680a7de655003688592
Jonathan Chancey <jchancey@ucmerced.edu>
2019-04-06 16:55:47 -0700

    all data accessed successfully

--- json test/jsontest.py
+++ json test/jsontest.py
@@ -1,18 +1,44 @@
 import json
 from jsoncomment import JsonComment
+from os.path import expanduser
 
 
-# Serialized JSON data written to a native Python str object.
-# json_string = json.dumps(data)
+# builds the path to the colors.json file we'll be reading from
+home = expanduser("~")
+colorfile = "/.cache/wal/colors.json"
+colorfile = home + colorfile
 
-# with open('distros.json', 'r') as f:
-#     distros_dict = json.load(f)
+# uses JsonComment as a our parser 
+parser = JsonComment(json)
 
-# for distro in distros_dict:
-#     print(distro['Name'])
 
-with open('settings.json', 'r') as f:
-    settings_dict = json.load(f)
+# try to load the colors into a dictionary
+# if fails, exit with error
+with open(colorfile, 'r') as fc:
+    color_dict = parser.load(fc)
 
-print(settings_dict['workbench.colorCustomizations']['sideBar.background'])
+print(color_dict)
+
+# create backup of original settings.json
+# try to load settings.json
+# if fails, exit with error
+# TODO change to non dummy dir
+# Should that be a user option?
+with open('settings.json', 'r') as fs:
+    settings_dict = parser.load(fs)
+
+print(settings_dict)
+
+
+# print(settings_dict['workbench.colorCustomizations']['sideBar.background'])
+
+# try to find all entries
+# if fails, appends the entries we need appropriate json section
+
+# take the colors and set the forground background, 
+# those of the sidebar, maybe the cursor.
+
+
+
+
 
---
+++ json test/scratch.py
@@ -0,0 +1,8 @@
+# Serialized JSON data written to a native Python str object.
+# json_string = json.dumps(data)
+
+# with open('distros.json', 'r') as f:
+#     distros_dict = json.load(f)
+
+# for distro in distros_dict:
+#     print(distro['Name'])
